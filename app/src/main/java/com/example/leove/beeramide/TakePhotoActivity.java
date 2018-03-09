e6a2aeb2e1a2eaf9bc461a432159aa8bfeecf8a5
leomani <xboxlive84320@hotmail.fr>
2018-03-08 23:12:22 -0500

    Des trucs

--- app/src/main/java/com/example/leove/beeramide/TakePhotoActivity.java
+++ app/src/main/java/com/example/leove/beeramide/TakePhotoActivity.java
@@ -1,10 +1,13 @@
 package com.example.leove.beeramide;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Debug;
+import android.os.Environment;
 import android.os.PersistableBundle;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.support.v4.content.FileProvider;
 import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -20,7 +23,11 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Random;
 
 /**
@@ -31,6 +38,8 @@ public class TakePhotoActivity extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
     public ArrayList<String> playerList = new ArrayList<String>();
+    private File imageFile;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -56,13 +65,14 @@ public class TakePhotoActivity extends AppCompatActivity {
         }
     }
 
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         // Check which request we're responding to
         if (requestCode == REQUEST_IMAGE_CAPTURE) {
             // Make sure the request was successful
             if (resultCode == RESULT_OK) {
-                Intent intent = new Intent(this, GameActivity.class);
+                Intent intent = new Intent(TakePhotoActivity.this, GameActivity.class);
                 intent.putStringArrayListExtra("playerList", playerList);
                 startActivity(intent);
             }
