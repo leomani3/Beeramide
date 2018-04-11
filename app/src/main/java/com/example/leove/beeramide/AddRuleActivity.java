13919492fad34ea3994cb078a438ae2899765077
leomani <xboxlive84320@hotmail.fr>
2018-04-11 14:39:45 -0400

    blblblbl

--- app/src/main/java/com/example/leove/beeramide/AddRuleActivity.java
+++ app/src/main/java/com/example/leove/beeramide/AddRuleActivity.java
@@ -24,37 +24,5 @@ public class AddRuleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_rule_activity);
 
-        //Reference à tous les widget
-        ListView lv = findViewById(R.id.player_list);
-        Button addPlayerBt = findViewById(R.id.add_player);
-        Button startGameBt = findViewById(R.id.start_game);
-        final EditText editText = findViewById(R.id.player_name);
-
-        //Création de la list de player
-        final ArrayList<String> playerList = new ArrayList<String>();
-        //Attache à l'ArrayAdapter
-        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.player_list_adapter, R.id.textView, playerList);
-        lv.setAdapter(arrayAdapter);
-
-        //Add player
-        addPlayerBt.setOnClickListener(new View.OnClickListener(){
-
-            public  void onClick(View view){
-                playerList.add(editText.getText().toString());
-                editText.setText("");
-                arrayAdapter.notifyDataSetChanged();
-            }
-        });
-
-        //Start game
-        /*startGameBt.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view){
-                Intent intent = new Intent(AddRuleActivity.this, TakePhotoActivity.class);
-                intent.putStringArrayListExtra("playerList", playerList);
-                intent.putExtra("gameMoment", "before");
-                startActivity(intent);
-            }
-        });*/
-
     }
 }
